00c1b943b9728b145865b3b3a964932388281ef0
lx051 <117721440+lx051@users.noreply.github.com>
2023-06-08 14:20:45 +0800

    Create jsxiashk.js

---
+++ jsxiashk.js
@@ -0,0 +1,70 @@
+[rewrite_local]
+^https?:\/\/43\.133\.187\.238\/addressx5\/index\.html url script-request-header xiashk.js
+
+^https?:\/\/43\.133\.187\.238\/addressx5\/index\.html url script-response-body xiashk.js
+
+^https?:\/\/xiashk\.com\/addressx5 url script-request-header xiashk.js
+
+^https?:\/\/xiashk\.com\/addressx5 url script-response-body xiashk.js
+
+^https?:\/\/43\.153\.65\.5\/addressx5\/index\.html url script-request-header xiashk.js
+
+https?:\/\/43\.153\.65\.5\/addressx5\/index\.html url script-response-body xiashk.js
+
+^https?:\/\/43\.133\.187\.238\/addressx5\/index\.html url script-request-header xiashk.js
+
+^https?:\/\/43\.133\.187\.238\/addressx5\/index\.html url script-response-body xiashk.js
+
+^https?:\/\/xsserve\.com\/addressx5 url script-request-header xiashk.js
+
+^https?:\/\/xsserve\.com\/addressx5 url script-response-body xiashk.js
+
+^https?:\/\/43\.153\.98\.179\/addressx5\/index\.html url script-request-header xiashk.js
+
+^https?:\/\/43\.153\.98\.179\/addressx5\/index\.html url script-response-body xiashk.js
+
+^https?:\/\/43\.133\.187\.238\/addressx5\/index\.html url script-request-header xiashk.js
+
+^https?:\/\/43\.133\.187\.238\/addressx5\/index\.html url script-response-body xiashk.js
+
+^https?:\/\/43\.153\.8\.115\/addressx5\/index\.html url script-request-header xiashk.js
+
+^https?:\/\/43\.153\.8\.115\/addressx5\/index\.html url script-response-body xiashk.js
+
+^https?:\/\/43\.153\.8\.115\/addressx5\/index\.html url script-request-header xiashk.js
+
+^https?:\/\/43\.153\.8\.115\/addressx5\/index\.html url script-response-body xiashk.js
+
+^https?:\/\/43\.133\.187\.238\/addressx5\/index\.html url url script-request-header xiashk.js
+
+^https?:\/\/43\.133\.187\.238\/addressx5\/index\.html url script-response-body xiashk.js
+
+#[mitm]
+#hostname =
+
+var Url = $request.url;
+function setQueryString(key, val) { 
+    var url = Url.split('?'), search=url[1];
+    var query = {};
+    if (search) {
+        search.split('&').forEach((item) => {
+            var arr = item.split('=');
+            query[arr[0]] = arr[1];
+        });
+    }
+    query[key] = val;
+    var queryArr = [];
+    for (var p in query) {
+        queryArr.push(p + '=' + query[p]);
+    }
+    return url[0]+'?'+queryArr.join('&');
+};
+
+
+console.log(Url);
+$done({ url:Url });
+
+//
+var body=$response.body;
+
+$done(body);
