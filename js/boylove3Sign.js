d89e6ed0d4d37963c56d9d2300c48e29a6083df7
lx051 <117721440+lx051@users.noreply.github.com>
2023-06-09 18:07:04 +0800

    Create boylove3Sign.js

---
+++ js/boylove3Sign.js
@@ -0,0 +1,188 @@
+const lk = new ToolKit(`boylove3签到`, `Boylove3SignIn`)
+
+const boylove3CookieKey = 'lkBoylove3CookieKey'
+
+const boylove3Cookie = lk.getVal(boylove3CookieKey, '')
+
+if (!lk.isExecComm) {
+
+    if (lk.isRequest()) {
+
+        getCookie()
+
+        lk.done()
+
+    } else {
+
+        // 构建boxjs数据写入订阅
+
+        lk.boxJsJsonBuilder({
+
+            "icons": [
+
+                "https://raw.githubusercontent.com/lowking/Scripts/master/doc/icon/hifinisignin-dark.png",
+
+                "https://raw.githubusercontent.com/lowking/Scripts/master/doc/icon/hifinisignin.png"
+
+            ],
+
+            "settings": [
+
+                {
+
+                    "id": boylove3CookieKey,
+
+                    "name": "boylove3 cookie",
+
+                    "val": "",
+
+                    "type": "text",
+
+                    "desc": "boylove3 cookie"
+
+                }
+
+            ],
+
+            "keys": [boylove3CookieKey]
+
+        }, {
+
+            "script_url": "https://github.com/lowking/Scripts/blob/master/boylove3/boylove3Sign.js",
+
+            "author": "@lowking",
+
+            "repo": "https://github.com/lowking/Scripts",
+
+        })
+
+        all()
+
+    }
+
+}
+
+function getCookie() {
+
+    if (lk.isGetCookie(/\/my.htm/)) {
+
+        if ($request.headers.hasOwnProperty('Cookie')) {
+
+            lk.setVal(boylove3CookieKey, $request.headers.Cookie)
+
+            lk.appendNotifyInfo('🎉成功获取boylove3签到cookie，可以关闭相应脚本')
+
+        } else {
+
+            lk.appendNotifyInfo('❌获取boylove3签到cookie失败')
+
+        }
+
+        lk.msg('')
+
+    }
+
+}
+
+async function all() {
+
+    if (boylove3Cookie == '') {
+
+        lk.execFail()
+
+        lk.appendNotifyInfo(`⚠️请先先根据脚本注释获取cookie`)
+
+    } else {
+
+        await signIn()
+
+    }
+
+    lk.msg(``)
+
+    lk.done()
+
+}
+
+function signIn() {
+
+    return new Promise((resolve, reject) => {
+
+        lk.log("开始签到")
+
+        const t = '签到'
+
+        let url = {
+
+            url: 'https://www.boylove3.cc/sg_sign.htm',
+
+            headers: {
+
+                cookie: boylove3Cookie,
+
+                "User-Agent": lk.userAgent
+
+            }
+
+        }
+
+        lk.post(url, (error, response, data) => {
+
+            try {
+
+                if (error) {
+
+                    lk.execFail()
+
+                    lk.appendNotifyInfo(`❌${t}失败，请稍后再试`)
+
+                } else {
+
+                    let msg = data.split(`<h4 class="card-title text-center mb-0">`)[1].split(`</i>`)[1].split("<")[0]
+
+                    if (msg) {
+
+                        lk.appendNotifyInfo(`🎉${msg.trim()}`)
+
+                        lk.log(msg.trim())
+
+                        resolve(`suc🎉${msg.trim()}`)
+
+                    } else {
+
+                        lk.execFail()
+
+                        lk.appendNotifyInfo(data)
+
+                    }
+
+                }
+
+            } catch (e) {
+
+                lk.logErr(e)
+
+                lk.log(`返回数据：${data}`)
+
+                lk.execFail()
+
+                lk.appendNotifyInfo(`❌${t}错误，请带上日志联系作者，或稍后再试`)
+
+                resolve(`fail`)
+
+            } finally {
+
+                resolve()
+
+            }
+
+        })
+
+    })
+
+}
+
+```
+
+注意：由于我没有boylove3.cc的账号，无法验证此脚本是否有效。请自行测试并根据实际情况进行调整。
+
