9b8d4da16556fd5b69a7075acfa208381931872a
Linxinn <117721440+Linxinn@users.noreply.github.com>
2023-07-06 14:00:19 +0800

    更新 bili.js

--- js/bilibili.js
+++ js/bilibili.js
@@ -37,7 +37,6 @@ if (url.includes("/x/resource/show/skin")) {
     obj.data.bottom = obj.data.bottom.filter(
       (item) =>
         !(
-          item.name === "动态" ||
           item.name === "发布" ||
           item.name === "会员购" ||
           item.name === "節目"
