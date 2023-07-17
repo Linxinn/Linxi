2f2365c4becd4e5326250b40d74fa629090a86ae
Linxinn <117721440+Linxinn@users.noreply.github.com>
2023-07-17 23:45:43 +0800

    更新 ymqd.js

--- js/ymqd.js
+++ js/ymqd.js
@@ -1,1 +1,35 @@
-1
+//项目名称：一木清单
+//下载地址：https://apps.apple.com/app/id1672171155
+//脚本作者：chxm1023
+//电报频道：https://t.me/linxinn
+//使用说明：非一次性解锁，要一直开着
+
+
+[rewrite_local]
+^https:\/\/yimutodo\.com\/api\/v\/mtop url script-response-body https://raw.githubusercontent.com/Linxinn/Linxi/main/js/ymqd.js
+
+[mitm]
+hostname = yimutodo.com
+
+
+
+var linxi = JSON.parse($response.body);
+linxi.data = {
+    "memberType" : "PERMANENT",
+    "totalStore" :10485746725,
+    "avatar" : "https://raw.githubusercontent.com/lx051/QuantumultX/main/icon/hyj.JPG",
+    "userId" : 88888,
+    "nickName" : "林夕",
+  //"memberExpiredTime": 1881706768000
+};
+linxi.projectList = [{
+    "creator": {
+      "memberType": "PERMANENT",
+      "memberExpiredTime": 1881706768000,
+    },
+  }];
+$done({body: JSON.stringify(linxi)});
+ //"totalStore"这个是存储云空间，
+//20GB=2117632
+//1000GB=1048609678
+//10000GB=10485746725
